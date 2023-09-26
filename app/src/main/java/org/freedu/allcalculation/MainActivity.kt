be1d76e7cbd960a36640f45ee03a84e0f0c148ae
Musfiqur Rahman <mushfiqur15-10872@diu.edu.bd>
2023-09-26 12:51:29 +0600

    drawer navigation

--- app/src/main/java/org/freedu/allcalculation/MainActivity.kt
+++ app/src/main/java/org/freedu/allcalculation/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import org.freedu.allcalculation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -18,6 +22,17 @@ class MainActivity : AppCompatActivity() {
 
         var navController = findNavController(R.id.fragmentContainerView)
         binding.bottombar.setupWithNavController(navController)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout, R.string.nav_open, R.string.nav_close)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+        }else super.onOptionsItemSelected(item)
     }
 
 }
